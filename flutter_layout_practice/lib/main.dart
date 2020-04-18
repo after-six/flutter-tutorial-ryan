daad0c8a2d578357e50f44bf3758965d9d27e4fd
RyanCho <mith1004@gmail.com>
2020-04-18 13:15:31 +0900

    Slivers SliverFillRemaining 2

--- flutter_layout_practice/lib/main.dart
+++ flutter_layout_practice/lib/main.dart
@@ -25,18 +25,29 @@ class SliverLayout extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: <Widget>[
+        SliverList(
+          delegate: SliverChildListDelegate(const [
+            ListTile(title: Text('First item')),
+            ListTile(title: Text('Second item')),
+            ListTile(title: Text('Third item')),
+            ListTile(title: Text('Fourth item')),
+          ]),
+        ),
         SliverFillRemaining(
           hasScrollBody: false,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              FlutterLogo(size: 600),
-              Text(
-                'This is some longest text that should be centered'
-                'together with the logo',
-                textAlign: TextAlign.center,
-              ),
-            ],
+          child: Container(
+            color: Colors.yellowAccent,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlutterLogo(size: 200),
+                Text(
+                  'This is some longest text that should be centered'
+                  'together with the logo',
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
           ),
         ),
       ],
