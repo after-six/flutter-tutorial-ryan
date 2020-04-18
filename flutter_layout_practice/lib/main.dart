2eda4009380f41eb438ba024958e9b77925450c0
RyanCho <mith1004@gmail.com>
2020-04-18 10:45:34 +0900

    Stack Positioned

--- flutter_layout_practice/lib/main.dart
+++ flutter_layout_practice/lib/main.dart
@@ -10,7 +10,34 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: InstrinsicLayout());
+        home: StackLayout());
+  }
+}
+
+class StackLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Stack')),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Material(
+            color: Colors.yellowAccent,
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Icon(Icons.star, size: 50),
+          ),
+          Positioned(
+            top: 340,
+            left: 250,
+            child: Icon(Icons.call, size: 50),
+          ),
+        ],
+      ),
+    );
   }
 }
 
